0c30ee40181ea99c5ac3f6e8c0407e2943d91bc1
Winnie Schwaid-Lindner <w@wn.ie>
2023-06-27 10:29:04 -0500

    add sys.argv

--- RunRawGitHub.py
+++ RunRawGitHub.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-import requests
-print('---caution: ONLY use with urls/files you trust---')
-inputurl = input('paste the GitHub url below:\n> ')
+import requests, sys
+try:
+    inputurl = sys.argv[1]
+except:
+    print('---caution: ONLY use with urls/files you trust---')
+    inputurl = input('paste the GitHub url below:\n> ')
 rawpyurl = inputurl.replace('github.com', 'raw.githubusercontent.com').replace('/blob', '')
 rawpycode = requests.get(rawpyurl)
 exec(rawpycode.text)
