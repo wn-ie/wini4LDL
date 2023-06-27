fe21b7faf53df5ac7bbf53c4f04c900420a5f63e
Winnie Schwaid-Lindner <w@wn.ie>
2023-06-27 10:12:29 -0500

    change regular github url to raw github

--- RunRawGitHub.py
+++ RunRawGitHub.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import requests
-rawpyurl = input('paste the github raw url below:\n> ')
+print('---caution: ONLY use with urls/files you trust---')
+inputurl = input('paste the GitHub url below:\n> ')
+rawpyurl = inputurl.replace('github.com', 'raw.githubusercontent.com').replace('/blob', '')
 rawpycode = requests.get(rawpyurl)
 exec(rawpycode.text)
