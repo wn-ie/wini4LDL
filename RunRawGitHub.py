7fd5cfc0f034fb55e471c732f9068c7aaccf0929
Winnie Schwaid-Lindner <w@wn.ie>
2023-08-07 15:33:37 -0500

    Update RunRawGitHub.py

--- RunRawGitHub.py
+++ RunRawGitHub.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-# to run me: py P:/ath/to/this/file.py
+# to run me on Windows: py P:/ath/to/this/file.py
 
 import requests, sys
 try:
     inputurl = sys.argv[1]
     subargs = sys.argv[2:]
-    # print(inputurl, subargs)
 except:
-    print('---caution: ONLY use with urls/files you trust---')
     inputurl = input('paste the GitHub url below:\n> ')
-rawpyurl = inputurl.replace('github.com', 'raw.githubusercontent.com').replace('/blob', '')
-rawpycode = requests.get(rawpyurl)
-exec(rawpycode.text)
+if '/WinnieSchLin/' in inputurl:
+    rawpyurl = inputurl.replace('github.com', 'raw.githubusercontent.com').replace('/blob', '')
+    rawpycode = requests.get(rawpyurl)
+    exec(rawpycode.text)
+else:
+    print('\n--- This file can ONLY be used for code written by Winnie Schwaid-Lindner.\n--- If you would like help running code from other trusted sources,\n    please reach out to me at w@wn.ie\n')
